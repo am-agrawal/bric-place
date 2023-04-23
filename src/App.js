ec9da3ec48a77dc3f2c85be4bfd7180c99e7a6a0
Aman Agrawal <582002aman@gmail.com>
2023-04-23 12:18:05 +0530

    Revert "install tailwind css"
    This reverts commit f68b212958bceea2b9722d8f8b980802bdeacd6b.

--- src/App.js
+++ src/App.js
@@ -32,14 +32,11 @@ function App() {
 					<Route path="/sign-up" element={<SignUp />} />
 					<Route path="/forgot-password" element={<ForgotPassword />} />
 					<Route path="/create-listing" element={<CreateListing />} />
-					<Route
-						path="/category/:categoryName/:listingId"
-						element={<Listing />}
-					/>
+					<Route path="/category/:categoryName/:listingId" element={<Listing />} />
 					<Route path="/contact/:landlordId" element={<Contact />} />
 					<Route path="/edit-listing/:listingId" element={<EditListing />} />
 				</Routes>
-				<div className="margin-at-bottom"></div>
+				<div className="mb"></div>
 				<Navbar />
 			</Router>
 
